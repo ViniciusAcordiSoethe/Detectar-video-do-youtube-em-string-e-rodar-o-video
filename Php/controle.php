5e62842c91dca41d6ad2fbac5d4da4aba494d663
Soethe <soethe42@gmail.com>
2022-03-17 20:58:07 -0300

    Primeiro Commit

---
+++ Php/controle.php
@@ -0,0 +1,10 @@
+<?php
+
+$html = '<p>Escuta esse bit da pesada <a title="Musica no Youtube" href="https://www.youtube.com/watch?v=enzAqfTOE1Y&amp;ab_channel=splash.mp3" target="_blank" rel="noopener">Musica no Youtube</a> , mas tem esse tbm <a title="Musica 2 " href="https://www.youtube.com/watch?v=W0OK7t1W9Nc&amp;ab_channel=MCMenordaVG-Topic" target="_blank" rel="noopener">Musica 2</a></p>';
+
+$htmlexplode = explode('"' , $html);
+
+parse_str(parse_url($html, PHP_URL_QUERY), $var);
+
+print_r($var);
+
---
+++ javascript/controle.js
@@ -0,0 +1,3 @@
+var html = '<p>Escuta esse bit da pesada <a title="Musica no Youtube" href="https://www.youtube.com/watch?v=enzAqfTOE1Y&amp;ab_channel=splash.mp3" target="_blank" rel="noopener">Musica no Youtube</a> , mas tem esse tbm <a title="Musica 2 " href="https://www.youtube.com/watch?v=W0OK7t1W9Nc&amp;ab_channel=MCMenordaVG-Topic" target="_blank" rel="noopener">Musica 2</a></p>';
+
+alert(html);
