cc75d87d96b041d0d2e1a3791ac4b4f10a64b10d
Felipe Róza <felipe.fr@msn.com>
2022-03-17 22:13:08 -0300

    ;D

--- Php/controle.php
+++ Php/controle.php
@@ -1,26 +1,6 @@
 <?php
 
-$html = '<p>Escuta esse bit da pesada <a title="Musica no Youtube" href="https://www.youtube.com/watch?v=enzAqfTOE1Y&amp;ab_channel=splash.mp3" target="_blank" rel="noopener">Musica no Youtube</a> , mas tem esse tbm <a title="Musica 2 " href="https://www.youtube.com/watch?v=W0OK7t1W9Nc&amp;ab_channel=MCMenordaVG-Topic" target="_blank" rel="noopener">Musica 2</a></p>';
+$html = '<p>Escuta esse bit da pesada <a title="Musica no Youtube" href="https://https://www.youtube.com/watch?v=1RN7FxZa2FI" target="_blank" rel="noopener">Musica no Youtube</a> , mas tem esse tbm <a title="Musica 2 " href="https://https://www.youtube.com/watch?v=cEflMmt3MzQ" target="_blank" rel="noopener">Musica 2</a> e esse tbm <a title="Musica 3 " href="https://www.youtube.com/watch?v=JDTGroZRybw" target="_blank" rel="noopener">Musica 2</a> novas</p>';
 
-$htmlexplode = explode('"' , $html);
-
-foreach ($htmlexplode as $string) {
-    $char = str_word_count($string, 1);
-    print_r($char);
-
-    echo '<br>';
-
-    $guarda = in_array("watch", $char);
-    if ($guarda == 1) {
-        $tamanho = count($char);
-        for ($i = 0; $i < $tamanho; $i++) {
-            if ($char[$i] == "v") {
-                print_r($char[$i+1]);
-            }
-        }
-    }
-
-    echo '<br>';
-
-}
-
+preg_match_all('~(?:https?://)?(?:www.)?(?:youtube.com|youtu.be)/(?:watch\?v=)?([^\s]+)~', $html, $match);
+$tamanhoArray = count($match);
--- Php/index.php
+++ Php/index.php
@@ -5,10 +5,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Detecta video</title>
+    <title>Detecta video PHP</title>
 </head>
 <body>
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/<?= $var['v']?>" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <?php
+        foreach($match[1] as $link) {
+    ?>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/<?= $link?>" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <?php
+        }
+    ?>
 </body>
 </html>
