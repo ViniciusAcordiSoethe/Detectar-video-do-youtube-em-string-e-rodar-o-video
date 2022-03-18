d5b03ffaddf1a9474d966b7b43b45e78c042a619
Soethe <soethe42@gmail.com>
2022-03-17 21:37:59 -0300

    Tentando fazer Pegar

--- Php/controle.php
+++ Php/controle.php
@@ -2,14 +2,8 @@
 
 $html = '<p>Escuta esse bit da pesada <a title="Musica no Youtube" href="https://www.youtube.com/watch?v=enzAqfTOE1Y&amp;ab_channel=splash.mp3" target="_blank" rel="noopener">Musica no Youtube</a> , mas tem esse tbm <a title="Musica 2 " href="https://www.youtube.com/watch?v=W0OK7t1W9Nc&amp;ab_channel=MCMenordaVG-Topic" target="_blank" rel="noopener">Musica 2</a></p>';
 
-$htmlexplode = explode('href="https://www.youtube.com/' , $html);
+parse_str(parse_url($html, PHP_URL_QUERY), $var);
 
-$urlimplode = implode($htmlexplode);
+var_dump($var);
 
-$url = $urlimplode;
-//parse_str(parse_url($html, PHP_URL_QUERY), $var);
-
-echo $urlimplode;
-
-var_dump($htmlexplode);
 
