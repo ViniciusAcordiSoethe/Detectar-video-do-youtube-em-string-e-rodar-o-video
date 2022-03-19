2e7fd70ebaf474d611dcb6b88b3d72d769a7e93e
Soethe <soethe42@gmail.com>
2022-03-19 10:40:00 -0300

    Feita algumas correções

--- javascript/controle.js
+++ javascript/controle.js
@@ -1,13 +1,11 @@
-var mostralink = document.getElementById('mostralink');
 var link = '<p>Escuta esse bit da pesada <a title="Musica no Youtube" href="https://www.youtube.com/watch?v=enzAqfTOE1Y&amp;ab_channel=splash.mp3" target="_blank" rel="noopener">Musica no Youtube</a> , mas tem esse tbm <a title="Musica 2 " href="https://www.youtube.com/watch?v=W0OK7t1W9Nc&amp;ab_channel=MCMenordaVG-Topic" target="_blank" rel="noopener">Musica 2</a></p>';
-mostralink.innerHTML = "URL = " + link ;
-var down = document.getElementById('resultadopegacodio');
+var resultado = document.getElementById('resultadopegacodio');
 
 function pegacodigo() {
     VID_REGEX = /(?:youtube(?:-nocookie)?\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
-    down.innerHTML = link.match(VID_REGEX)[1];
+    resultado.innerHTML = link.match(VID_REGEX)[1];
 }
 
 function mostrartodos(){
-    
+
 }
